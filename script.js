4127b90d2ba53c4bfebda78f5d0d8b5501080934
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-17 18:14:28 +0300

    add chapter

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // get the aprent element
-var booksContainer = document.querySelector(".books");
-
+const booksContainer = document.querySelector(".books");
+const li = document.createElement("li");
 // function for getting the book number value from the element
 function getBookNumber(ele) {
   return Number(ele.querySelector("a").textContent.trim()[6]) || 0;
 }
-
 
 function sortElements(parentEl) {
   Array.from(parentEl.children)
@@ -15,10 +14,9 @@ function sortElements(parentEl) {
     .forEach(function (ele) {
       parentEl.appendChild(ele);
     });
-};
+}
 
 sortElements(booksContainer);
-
 
 // Заменить картинку заднего фона на другую из папки image
 const body = document.querySelector("body");
@@ -35,18 +33,33 @@ promout.remove();
 const a = booksContainer.children[2].querySelector("a");
 a.text = "Книга 3. this и Прототипы Объектов";
 
-
 // Sort books 2, 5 chapters
-function sortChapters(bookEl){
-  let parts = bookEl.querySelector("ul") 
-  Array.from(parts.children).slice(2).filter(el=> el.innerText.startsWith("Глава")).sort(function (a, b) { return Number(a.innerText[6]) - Number(b.innerText[6])}).forEach(function (ele) {
-    parts.appendChild(ele);
-  });
+function sortChapters(bookEl) {
+  let parts = bookEl.querySelector("ul");
+  Array.from(parts.children)
+    .slice(2)
+    .filter((el) => el.innerText.startsWith("Глава"))
+    .sort(function (a, b) {
+      return Number(a.innerText[6]) - Number(b.innerText[6]);
+    })
+    .forEach(function (ele) {
+      parts.appendChild(ele);
+    });
 
-  Array.from(parts.children).slice(2).filter(el=> el.innerText.startsWith("Приложение")).sort((a, b)=> a.innerText.localeCompare(b.innerText)).forEach(function (ele) {
-    parts.appendChild(ele);
-  });
+  Array.from(parts.children)
+    .slice(2)
+    .filter((el) => el.innerText.startsWith("Приложение"))
+    .sort((a, b) => a.innerText.localeCompare(b.innerText))
+    .forEach(function (ele) {
+      parts.appendChild(ele);
+    });
 }
 
-sortChapters(booksContainer.children[1])
-sortChapters(booksContainer.children[4])
+sortChapters(booksContainer.children[1]);
+sortChapters(booksContainer.children[4]);
+//в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+
+li.appendChild(document.createTextNode("Глава 8: За пределами ES6"));
+booksContainer.children[5].children[1].appendChild(li);
+sortChapters(booksContainer.children[5]);
+
